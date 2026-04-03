dbd4bd4d92a7af1db090b668a7f075f33a7c762f
erik <khechumyanerik1@gmail.com>
2026-04-03 17:18:24 +0400

    fix bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,31 @@ const _inter = Inter({ subsets: ["latin", "cyrillic"] })
 
 export const metadata: Metadata = {
   title: 'FortisTeach - IT, Network & Smart Technology Services in Armenia',
-  description: 'FortisTeach-ը առաջարկում է պրոֆեսիոնալ IT ծառայություններ Հայաստանում՝ ներառյալ ցանցերի կարգավորում, սերվերների տեղադրում, VPN լուծումներ, տեսահսկման համակարգեր և Smart Home տեխնոլոգիաներ: IT services Armenia, IT support Yerevan, IT infrastructure services, IT consulting Armenia, network setup Armenia, VPN setup Armenia, MikroTik configuration, Cisco network configuration, server setup Armenia, NAS storage setup, Active Directory setup, virtualization server setup, Linux server configuration, CCTV installation Yerevan, IP camera installation Armenia, Smart Home installation Armenia, home automation system Armenia, cloud infrastructure setup.',
+  description: 'FortisTeach-ը առաջարկում է պրոֆեսիոնալ IT ծառայություններ Հայաստանում՝ ներառյալ ցանցերի կարգավորում, սերվերների տեղադրում, VPN լուծումներ, տեսահսկման համակարգեր և Smart Home տեխնոլոգիաներ.',
+  
+  openGraph: {
+    title: 'FortisTeach - IT, Network & Smart Technology Services in Armenia',
+    description: 'FortisTeach-ը առաջարկում է պրոֆեսիոնալ IT ծառայություններ Հայաստանում՝ ներառյալ ցանցերի կարգավորում, սերվերների տեղադրում, VPN լուծումներ, տեսահսկման համակարգեր և Smart Home տեխնոլոգիաներ.',
+    url: 'https://www.fortisteach.com/',
+    siteName: 'FortisTeach',
+    images: [
+      {
+        url: 'https://www.fortisteach.com/icon.svg', 
+        width: 1200,
+        height: 630,
+        alt: 'FortisTeach Logo',
+      },
+    ],
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'FortisTeach - IT, Network & Smart Technology Services in Armenia',
+    description: 'FortisTeach-ը առաջարկում է պրոֆեսիոնալ IT ծառայություններ Հայաստանում՝ ներառյալ ցանցերի կարգավորում, սերվերների տեղադրում, VPN լուծումներ, տեսահսկման համակարգեր և Smart Home տեխնոլոգիաներ.',
+    images: ['https://www.fortisteach.com/logo.png'],
+  },
+
   icons: {
     icon: [
       {
